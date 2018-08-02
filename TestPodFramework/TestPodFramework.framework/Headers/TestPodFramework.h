664739474d11fcc1652c89d4f4445d5d63531126
zhoulianming <zhoulianming@miao.cn>
2018-08-02 13:32:03 +0800

    提交framework

---
+++ TestPodFramework/TestPodFramework.framework/Headers/TestPodFramework.h
@@ -0,0 +1,21 @@
+//
+//  TestPodFramework.h
+//  TestPodFramework
+//
+//  Created by 088 on 2018/8/2.
+//  Copyright © 2018年 088. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestPodFramework.
+FOUNDATION_EXPORT double TestPodFrameworkVersionNumber;
+
+//! Project version string for TestPodFramework.
+FOUNDATION_EXPORT const unsigned char TestPodFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestPodFramework/PublicHeader.h>
+
+
+#import <TestPodFramework/TestPodFrameworkManager.h>
+
